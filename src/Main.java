d04b4f6b5fcd3c4ed107a6847a10c6112e58161c
Veronazavr <verochka10@list.ru>
2020-08-15 19:31:57 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 2_000_000_000;
+        int remittance = 500_000_000;
+        int total = balance + remittance;
+        System.out.println(total);
+    }
+
+
+
+
+}
